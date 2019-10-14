72fe8b5532177275e7f418478b6f5155f3c410df
Dan Suh <kaehops@gmail.com>
2019-10-15 02:29:16 +0900

    preprocess function implemented

--- jdc/dataset.py
+++ jdc/dataset.py
@@ -1,6 +1,9 @@
-import json
 from collections import namedtuple
-import torch
+import json
+import os
+
+import librosa
+import numpy as np
 import torch.utils.data.dataset as dataset
 
 # represents metadata for single source (mix)
@@ -8,6 +11,20 @@ SourceInfo = namedtuple(
     'SourceInfo',
     ['fname', 'audio_path', 'melody1_path', 'melody2_path', 'melody3_path',
      'artist', 'title', 'genre', 'is_excerpt', 'is_instrumental', 'n_sources'])
+
+
+def medleydb_preprocess(root: str, target_sr=8000):
+    for f in os.listdir(root):
+        fpath = os.path.join(root, f)
+        print(fpath)
+        y, sr = librosa.load(fpath)
+
+        # resample
+        y_resamp = librosa.resample(y, orig_sr=sr, target_sr=target_sr)
+
+        mag_spec = librosa.stft(y_resamp, n_fft=1024, hop_length=80, win_length=1024)
+        log_mag = np.log(mag_spec)
+        print(log_mag.shape)
 
 
 class MedleyDBMelodyDataset(dataset.Dataset):
@@ -43,3 +60,4 @@ class MedleyDBMelodyDataset(dataset.Dataset):
 
 if __name__ == '__main__':
     db = MedleyDBMelodyDataset('/Users/dansuh/datasets/MedleyDB-Melody/medleydb_melody_metadata.json')
+    medleydb_preprocess('/Users/dansuh/datasets/MedleyDB-Melody/audio')
